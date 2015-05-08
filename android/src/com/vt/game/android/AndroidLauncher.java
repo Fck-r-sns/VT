68c6acb56f974ad03d19d1171053c65307489bd4
Fck-r-sns <fck.r.sns@gmail.com>
2015-05-08 22:00:51 +0300

    Android project added

---
+++ android/src/com/vt/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vt.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vt.game.VTGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VTGame(), config);
+	}
+}

a89ebb4e66c2619f90661826b8a2493da02abf15
Fck-r-sns <fck.r.sns@gmail.com>
2015-07-09 12:33:18 +0300

    Status bar is hiden now

--- android/src/com/vt/game/android/AndroidLauncher.java
+++ android/src/com/vt/game/android/AndroidLauncher.java
@@ -7,10 +7,11 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.vt.game.VTGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new VTGame(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.hideStatusBar = true;
+        initialize(new VTGame(), config);
+    }
 }

03650cbf2e4ddfe67ce3c90e2628b7090978a0fa
Fck-r-sns <fck.r.sns@gmail.com>
2016-01-15 22:00:37 +0300

    IndexedPriorityQueue<K, V> implemented

--- core/src/com/vt/logic/pathfinding/IndexedPriorityQueue.java
+++ core/src/com/vt/logic/pathfinding/IndexedPriorityQueue.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
  * Created by fckrsns on 10.01.2016.
  */
 
-public class IndexedPriorityQueue<K> {
-    public static class Result<K> {
+public class IndexedPriorityQueue<K, V extends Comparable<V>> {
+    public static class Result<K, V> {
         public K key;
-        public float priority;
+        public V priority;
 
-        public Result(K key, float priority) {
+        public Result(K key, V priority) {
             this.key = key;
             this.priority = priority;
         }
@@ -19,19 +19,19 @@ public class IndexedPriorityQueue<K> {
 
     private int m_capacity;
     private int m_size;
-    private float[] m_heap;
+    private V[] m_heap;
     private HashMap<K, Integer> m_keyToHeapPos;
     private K[] m_heapPosToKey;
 
     public IndexedPriorityQueue(int capacity) {
         m_capacity = capacity;
         m_size = 0;
-        m_heap = new float[m_capacity];
+        m_heap = (V[])new Comparable[m_capacity];
         m_keyToHeapPos = new HashMap<K, Integer>(m_capacity);
         m_heapPosToKey = (K[])new Object[m_capacity];
     }
 
-    public void insert(K key, float priority) {
+    public void insert(K key, V priority) {
         if (m_size >= m_capacity)
             throw new IndexOutOfBoundsException("No more capacity");
         m_heap[m_size] = priority;
@@ -42,31 +42,31 @@ public class IndexedPriorityQueue<K> {
         ++m_size;
     }
 
-    public float getPriority(K key) {
+    public V getPriority(K key) {
         int pos = m_keyToHeapPos.get(key);
         return m_heap[pos];
     }
 
-    public void changePriority(K key, float value) {
+    public void changePriority(K key, V value) {
         int pos = m_keyToHeapPos.get(key);
-        if (m_heap[pos] > value)
+        if (m_heap[pos].compareTo(value) > 0)
             goDown(pos);
         else
             goUp(pos);
     }
 
-    public Result<K> deleteMin() {
+    public Result<K, V> deleteMin() {
         if (isEmpty())
             throw new IndexOutOfBoundsException("Queue is empty");
         K key = m_heapPosToKey[0];
-        float priority = m_heap[0];
+        V priority = m_heap[0];
         --m_size;
         if (!isEmpty()) {
             swap(0, m_size);
             if (m_size > 1)
                 goDown(0);
         }
-        return new Result<K>(key, priority);
+        return new Result<K, V>(key, priority);
     }
 
     public boolean isEmpty() {
@@ -78,7 +78,7 @@ public class IndexedPriorityQueue<K> {
     }
 
     private void swap(int heapPos1, int heapPos2) {
-        float tmp = m_heap[heapPos1];
+        V tmp = m_heap[heapPos1];
         m_heap[heapPos1] = m_heap[heapPos2];
         m_heap[heapPos2] = tmp;
         K key1 = m_heapPosToKey[heapPos1];
@@ -92,7 +92,7 @@ public class IndexedPriorityQueue<K> {
     private void goUp(int pos) {
         do {
             int parentPos = (pos - 1) / 2;
-            if (m_heap[pos] >= m_heap[parentPos])
+            if (m_heap[pos].compareTo(m_heap[parentPos]) >= 0)
                 break;
             swap(pos, parentPos);
             pos = parentPos;
@@ -109,8 +109,8 @@ public class IndexedPriorityQueue<K> {
             if (rightChild >= m_size)
                 minChild = leftChild; // no right child
             else
-                minChild = (m_heap[leftChild] <= m_heap[rightChild]) ? leftChild : rightChild;
-            if (m_heap[pos] <= m_heap[minChild])
+                minChild = (m_heap[leftChild].compareTo(m_heap[rightChild]) <= 0) ? leftChild : rightChild;
+            if (m_heap[pos].compareTo(m_heap[minChild]) <= 0)
                 break;
             swap(pos, minChild);
             pos = minChild;
